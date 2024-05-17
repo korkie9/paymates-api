4afeaa9da3c981981e3f637df4488f104f18c524
Justin korkie <justin@Hiyori.hiyori.com>
2024-05-17 23:53:23 +0200

    feat(user): add feat for adding photo

--- paymatesapi/Controllers/UserController.cs
+++ paymatesapi/Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using Azure.Identity;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using paymatesapi.Entities;
@@ -26,6 +28,43 @@ namespace paymatesapi.Controllers
         {
             BaseResponse<bool> response = await _userAuthService.UpdateUser(userRequest);
             return response.Error != null ? BadRequest(response) : Ok(response);
+        }
+
+        [HttpPost("upload-photo")]
+        public async Task<IActionResult> UploadBlob(IFormFile file)
+        {
+            try
+            {
+                if (file == null || file.Length == 0)
+                    return BadRequest("File is empty");
+                string containerName = "photos";
+                // Get a reference to the container
+                var blobServiceClient = new BlobServiceClient(
+                    new Uri("https://paymates.blob.core.windows.net"),
+                    new DefaultAzureCredential()
+                );
+                var blobContainerClient = blobServiceClient.GetBlobContainerClient(containerName);
+
+                // Generate a unique blob name or use a predefined name
+                var blobName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                // Get a reference to the blob
+                var blobClient = blobContainerClient.GetBlobClient(blobName);
+
+                // Upload the file to the blob storage
+                using (var stream = file.OpenReadStream())
+                {
+                    await blobClient.UploadAsync(stream, true);
+                }
+
+                // Return the blob URI
+                var blobUri = blobClient.Uri.ToString();
+                return Ok(blobUri);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred: {ex.Message}");
+            }
         }
     }
 }
